21c8e12437bc005c3cab430d029740db8fc08c29
Krijn Schelvis <kschelvi@f1r5s9.codam.nl>
2024-06-11 12:58:57 +0200

    commit

--- command.c
+++ command.c
@@ -1,20 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
+/*   command.c                                          :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: kschelvi <kschelvi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2024/06/03 15:03:27 by kschelvi      #+#    #+#                 */
-/*   Updated: 2024/06/11 12:30:32 by kschelvi      ########   odam.nl         */
+/*   Created: 2024/06/11 12:16:04 by kschelvi      #+#    #+#                 */
+/*   Updated: 2024/06/11 12:33:29 by kschelvi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "my_error.h"
 #include "lexer.h"
-#include <stdlib.h>
+#include "libft.h"
 
-int main(void)
+#include <stdbool.h>
+
+
+t_command	*get_commands(t_usr_input input)
 {
-	lexer(NULL, "cat test.txt | wc -l");
+	t_command	*commands;
+
+	commands = (t_command *)ft_calloc(1, sizeof(t_command));
+	if (!commands)
+		return (NULL);
+	commands->pipe_data = (t_pipe_data *)ft_calloc(1, sizeof(t_pipe_data));
+	if (!commands->pipe_data)
+		return (free(commands), NULL);
 }
--- tokenizer.c
+++ tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: kschelvi <kschelvi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/06 13:33:19 by kschelvi      #+#    #+#                 */
-/*   Updated: 2024/06/06 15:07:21 by kschelvi      ########   odam.nl         */
+/*   Updated: 2024/06/11 12:13:20 by kschelvi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,48 +37,48 @@ static t_token_type	get_type(t_arg arg, char *str)
 }
 
 void free_tokens(t_token **head) {
-    t_token *current;
-    t_token *tmp;
+	t_token *current;
+	t_token *tmp;
 
-    current = *head;
-    while (current) {
-        if (current->str) {
-            free(current->str);
-        }
-        tmp = current;
-        current = current->next;
-        free(tmp);
-    }
-    *head = NULL;
+	current = *head;
+	while (current)
+	{
+		if (current->str)
+			free(current->str);
+		tmp = current;
+		current = current->next;
+		free(tmp);
+	}
+	*head = NULL;
 }
 
 t_error tokenizer(t_usr_input *usr_input)
 {
-    t_token *head;
+	t_token *head;
 	t_token	*current_tok;
-    t_arg   *current_arg;
+	t_arg   *current_arg;
 
-    head = (t_token *)ft_calloc(1, sizeof(t_token));
-    if (!head)
-        return (ERR_MALLOC);
-    current_tok = head;
-    current_arg = usr_input->args;
-    while (current_arg)
-    {
-        current_tok->str = ft_substr(usr_input->str, current_arg->start, current_arg->end - current_arg->start + 1);
-        if (!current_tok->str)
-            return (free_tokens(&head), ERR_MALLOC);
-        current_tok->type = get_type(*current_arg, current_tok->str);
-        if (current_arg->next)
+	head = (t_token *)ft_calloc(1, sizeof(t_token));
+	if (!head)
+		return (ERR_MALLOC);
+	current_tok = head;
+	current_arg = usr_input->args;
+	while (current_arg)
+	{
+		current_tok->str = ft_substr(usr_input->str, current_arg->start, current_arg->end - current_arg->start + 1);
+		if (!current_tok->str)
+			return (free_tokens(&head), ERR_MALLOC);
+		current_tok->type = get_type(*current_arg, current_tok->str);
+		if (current_arg->next)
 		{
-            current_tok->next = (t_token *)ft_calloc(1, sizeof(t_token));
-            if (!current_tok->next)
-                return (free_tokens(&head), ERR_MALLOC);
-            current_tok = current_tok->next;
-        }
-        current_arg = current_arg->next;
-    }
-    current_tok = NULL;
-    usr_input->token = head;
-    return (ERR_OK);
+			current_tok->next = (t_token *)ft_calloc(1, sizeof(t_token));
+			if (!current_tok->next)
+				return (free_tokens(&head), ERR_MALLOC);
+			current_tok = current_tok->next;
+		}
+		current_arg = current_arg->next;
+	}
+	current_tok = NULL;
+	usr_input->token = head;
+	return (ERR_OK);
 }
